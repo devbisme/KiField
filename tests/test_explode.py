102e935157042e99f66fc80168aa8472631a2e60
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-21 19:04:56 +0000

    Refactor test_explode

--- tests/test_explode.py
+++ tests/test_explode.py
@@ -65,42 +65,6 @@ class TestExplode(unittest.TestCase):
         self.assertEqual(refs, [])
 
 
-def toRef(part):
-    return '{}{}'.format(part[0], part[1])
-
-
-@st.composite
-def random_prefix(draw):
-    #limited to unicode letters, see
-    #https://en.wikipedia.org/wiki/Unicode_character_property#General_Category
-    categories = ['Ll', 'Lt', 'Lm', 'Lo']
-    characters = st.lists(st.characters(whitelist_categories=categories), min_size=1)
-    prefix = st.text(alphabet = draw(characters), min_size=1)
-    return draw(prefix)
-
-
-@st.composite
-def random_reference(draw, prefix = random_prefix()):
-    number = st.integers(min_value = 0)
-    return draw(st.tuples(prefix, number))
-
-
-@st.composite
-def totally_random_references(draw):
-    '''generates random lists of references like ['IASDHAH1', 'AKJDJAD1569', ...]'''
-    parts = draw(st.lists(random_reference()))
-    parts.sort()
-    return list(map(toRef, parts))
-
-
-@st.composite
-def random_references(draw):
-    '''generates random lists of references width the same prefix like ['IASDHAH1', 'IASDHAH1569', ...]'''
-    prefix = st.just(draw(random_prefix()))
-    parts = draw(st.lists(random_reference(prefix = prefix)))
-    parts.sort()
-    return list(map(toRef, parts))
-
 
 class TestCollapse(unittest.TestCase):
     def test_collapses(self):
@@ -119,23 +83,66 @@ class TestCollapse(unittest.TestCase):
         collapsed = kifield.collapse([])
         self.assertEqual(collapsed, '')
 
+
+
+def toRef(part):
+    return '{}{}'.format(part[0], part[1])
+
+
+@st.composite
+def random_prefix(draw):
+    #limited to unicode letters, see
+    #https://en.wikipedia.org/wiki/Unicode_character_property#General_Category
+    categories = ['Ll', 'Lt', 'Lm', 'Lo']
+    characters = st.lists(st.characters(whitelist_categories=categories), min_size = 1)
+    prefix = st.text(alphabet = draw(characters), min_size = 1)
+    return draw(prefix)
+
+
+@st.composite
+def random_reference(draw, prefix = random_prefix()):
+    number = st.integers(min_value = 0)
+    return draw(st.tuples(prefix, number))
+
+
+@st.composite
+def totally_random_references(draw):
+    '''generates random sorted lists of references like ['IASDHAH1', 'AKJDJAD1569', ...]'''
+    parts = draw(st.lists(random_reference()))
+    parts.sort()
+    return list(map(toRef, parts))
+
+
+@st.composite
+def random_references(draw):
+    '''generates random sorted lists of references width the same prefix like ['IASDHAH1', 'IASDHAH1569', ...]'''
+    prefix = st.just(draw(random_prefix()))
+    parts = draw(st.lists(random_reference(prefix = prefix)))
+    parts.sort()
+    return list(map(toRef, parts))
+
+
+
+max_examples = 200
+
+class TestExplodeCollapseProperties(unittest.TestCase):
     @hypothesis.given(random_references())
-    @hypothesis.settings(max_examples=200)
+    @hypothesis.settings(max_examples = max_examples)
     def test_explode_is_inverse(self, references):
         assert kifield.explode(kifield.collapse(references)) == references
 
     @hypothesis.given(random_references())
-    @hypothesis.settings(max_examples=200)
+    @hypothesis.settings(max_examples = max_examples)
     def test_explode_is_inverse2(self, references):
         assert kifield.collapse(kifield.explode(kifield.collapse(references))) == kifield.collapse(references)
 
     @hypothesis.given(totally_random_references())
-    @hypothesis.settings(max_examples=200)
+    @hypothesis.settings(max_examples = max_examples)
     def test_explode_is_inverse(self, references):
         assert kifield.explode(kifield.collapse(references)) == references
 
     @hypothesis.given(totally_random_references())
-    @hypothesis.settings(max_examples=200)
+    @hypothesis.settings(max_examples = max_examples)
     def test_explode_is_inverse2(self, references):
         assert kifield.collapse(kifield.explode(kifield.collapse(references))) == kifield.collapse(references)
 
